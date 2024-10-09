53a42ec87c3cdce242734f77cab73cdfad007068
Kjanas1 <134191929+Kjanas1@users.noreply.github.com>
2024-10-09 00:49:06 -0500

    10/8-10/9

---
+++ scripts/quiz.js
@@ -0,0 +1,138 @@
+// order of things
+// 3. call questions
+
+function setQuizChoice() {
+    if (localStorage.getItem("quizChoice") == 0) {
+        console.log("error");
+        //load redirect function
+    }
+    else {
+        // sessionStorage.setItem("quizChoice",`quiz${localStorage.getItem("quizChoice")}`);
+        sessionStorage.setItem("quizChoice", localStorage.getItem("quizChoice"));
+        localStorage.setItem("quizChoice", 0);
+        getQuiz();
+    }
+};
+
+function getQuiz() {
+    //retrieve JSON
+    // endpoint address & variable
+    let serverEndpoint = "https://f93a4022-8613-4164-b8f8-36749f0f90d4.mock.pstmn.io/quiz1";
+    let endpoint = `${serverEndpoint}`;
+
+    // XMLHttp Resquest
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", function (data) {
+        if (this.status === 200) {
+            // display quiz
+
+            //stores quiz data in SessionStorage
+            sessionStorage.setItem("quizData", JSON.stringify(this.response))
+
+        } else {
+            // display an error message 
+            // document.getElementById("facts").innerHTML = "<p>Our website encountered an issue retrieving today's Bearded Dragon Facts, but rest assured, Beared dragons are still great.</p>";
+        }
+    });
+
+    // set the expected response type
+    xhr.responseType = "json";
+
+    // open a connection to the endpoint
+    xhr.open("GET", endpoint);
+
+    // send the request
+    xhr.send();
+};
+
+let questions = [
+    {
+        title: 'gato',
+        alternatives: ['dog', 'cat', 'bird', 'fish'],
+        correctAnswer: 1
+    },
+    {
+        title: 'ave',
+        alternatives: ['mouse', 'hamster', 'lizard', 'bird'],
+        correctAnswer: 3
+    },
+    {
+        title: 'rata',
+        alternatives: ['cat', 'fish', 'rat', 'shark'],
+        correctAnswer: 2
+    },
+    {
+        title: 'mosca',
+        alternatives: ['fly', 'puma', 'fish', 'dog'],
+        correctAnswer: 0
+    }
+];
+
+let app = {
+    start: function () {
+        var quizJson = JSON.parse(sessionStorage.getItem("quizData"));
+
+        this.currPosition = 0;
+
+        let titleDiv = document.getElementById('title');
+        // titleDiv.textContent = quizJson.title;
+        // titleDiv.textContent = question.title;
+
+        let answerButtons = document.querySelectorAll('.button');
+        answerButtons.forEach(function (element, index) {
+            element.addEventListener("click", function () {
+                //check for correct answer
+                this.checkAnswer(index);
+
+            }.bind(this));
+        }.bind(this)); //needed to pass context along to the object
+
+        //show first question
+        // this.showQuestion(quizJson);
+        this.showQuestion(questions[this.currPosition]);
+    },
+    showQuestion: function (q) {
+
+        //show question
+        let questionDesc = document.getElementById("questionText")
+        // questionDesc.textContent = q.questions.q1.desc;
+        questionDesc.textContent = q.title;
+
+        //show answers
+        let answers = document.querySelectorAll('.button');
+        answers.forEach(function (element, index) {
+            // element.textContent = q.questions.q1.answers[index];
+            element.textContent = q.alternatives[index];
+        })
+    },
+    checkAnswer: function (userSelected) {
+
+        let currQuestion = questions[this.currPosition];
+
+        if (currQuestion.correctAnswer == userSelected) {
+            //correct
+            console.log('correct')
+        }
+        else {
+            //incorrect
+            console.log("wrong")
+        }
+        //increase position
+        this.increasePosition();
+
+        //show next question
+        this.showQuestion(questions[this.currPosition]);
+
+    },
+    increasePosition: function () {
+        this.currPosition++;
+
+        if (this.currPosition == questions.length) {
+            this.currPosition = 0;
+        }
+    }
+};
+
+setQuizChoice()
+app.start();
---
+++ scripts/quizSelectionStandard.js
@@ -0,0 +1,55 @@
+console.log("hi")
+let x = 50;
+
+
+
+function quizDirect(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // if (e === document.getElementById("quiz1Button")){
+    if (e === document.getElementById("quiz1Button")) {
+        localStorage.setItem("quizChoice", 1);
+    }
+    else if (e == document.getElementById("quiz2Button")) {
+        localStorage.setItem("quizChoice", 2);
+    }
+    else if (e == document.getElementById("quiz3Button")) {
+        localStorage.setItem("quizChoice", 3);
+    }
+    else {
+        localStorage.setItem("quizChoice", 0);
+    }
+
+    //if e = specific element, then...
+    //store specific variable in local
+    // e = specific element 2, then...
+    //etc.
+    //else...
+    // store non-quiz variable
+    //"no quiz available, please select a quiz from the list, redirect back to page"
+
+}
+function quiz1Direct(e){
+    // e.preventDefault(); //because I want it to redirect anyway
+    e.stopPropagation();
+    localStorage.setItem("quizChoice", 1);
+}
+function quiz2Direct(e){
+    // e.preventDefault(); //because I want it to redirect anyway
+    e.stopPropagation();
+    localStorage.setItem("quizChoice", 2);
+}
+function quiz3Direct(e){
+    // e.preventDefault(); //because I want it to redirect anyway
+    e.stopPropagation();
+    localStorage.setItem("quizChoice", 3);
+}
+
+// document.getElementsByClassName("button").addEventListener("click", quizDirect);
+document.getElementById("quiz1Button").addEventListener("click", quiz1Direct);
+document.getElementById("quiz2Button").addEventListener("click", quiz2Direct);
+document.getElementById("quiz3Button").addEventListener("click", quiz3Direct);
+window.onload = function () {
+    localStorage.setItem("quizChoice",0);
+};
